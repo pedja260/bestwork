a478f69f7fb2172c01e53164de8f482ec9c8e6f3
pedja260 <pedja260@gmail.com>
2022-02-12 11:28:38 +0100

    new work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
-import NewComponent from "./NewComponent";
+import NewWork from "./components/NewWork";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>App component</h1>
-                <NewComponent />
+                <NewWork />
             </div>
 
         )
---
+++ src/components/NewWork.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+ class NewWork extends Component {
+     render() {
+         return (
+             <div>
+                 <h1>New Work</h1>
+             </div>
+         )
+     }
+ }
+
+ export default NewWork;
