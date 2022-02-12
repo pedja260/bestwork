a8876f64fda68a2049d92a3bcea32aa2ddb0558f
pedja260 <pedja260@gmail.com>
2022-02-12 11:13:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import NewComponent from "./NewComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App component</h1>
+                <NewComponent />
+            </div>
+
+        )
+    }
+}
+
+export default App;
